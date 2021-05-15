1e609a790154161ea978d571835a8cd7999bfbcd
StacieKot <stacie.kot@gmail.com>
2021-05-15 17:44:37 +0300

    commit 37

--- js/script.js
+++ js/script.js
@@ -329,7 +329,7 @@ function createApp() {
         this.board.clearFullRows();
         if (this.board.fullRowsNum) {
           this.playAudio(this.audioClearRows);
-          window.navigator.vibrate([130, 50, 130]);
+          window.navigator.vibrate([200, 100, 200, 100, 200]);
           this.updateScore();
           this.updateLevel();
         }
